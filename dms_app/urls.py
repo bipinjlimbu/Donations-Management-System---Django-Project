a45c84dbd63cbf692208066f6557c3143b6ce6f4
bipinjlimbu <bipinlimbu712@gmail.com>
2026-02-18 21:00:02 +0545

    Approve Signup Routing Done

--- dms_app/urls.py
+++ dms_app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from .views.auth import register_view, login_view
-from .views.main import home_view, admin_dashboard_view
+from .views.main import home_view, admin_dashboard_view, approve_signup_request
 
 urlpatterns = [
     path("",home_view,name="home"),
     path("register/",register_view,name="register"),
     path("login/",login_view,name="login"),
     path("dashboard/admin/",admin_dashboard_view,name="admin-dashboard"),
+    path("approve_signup_request/<int:request_id>/", approve_signup_request, name="approve_signup_request"),
 ]
