9c986a896706194bf6867323e71eac3aafd30c46
bipinjlimbu <bipinlimbu712@gmail.com>
2026-02-18 21:06:21 +0545

    Reject Signup Routing Done

--- dms_app/urls.py
+++ dms_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views.auth import register_view, login_view
-from .views.main import home_view, admin_dashboard_view, approve_signup_request
+from .views.main import home_view, admin_dashboard_view, approve_signup_request, reject_signup_request
 
 urlpatterns = [
     path("",home_view,name="home"),
@@ -8,4 +8,5 @@ urlpatterns = [
     path("login/",login_view,name="login"),
     path("dashboard/admin/",admin_dashboard_view,name="admin-dashboard"),
     path("approve_signup_request/<int:request_id>/", approve_signup_request, name="approve_signup_request"),
+    path("reject_signup_request/<int:request_id>/", reject_signup_request, name="reject_signup_request"),
 ]
