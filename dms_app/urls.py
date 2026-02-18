14ab210c198248c3f88c798ffe412ec2f6ef708f
bipinjlimbu <bipinlimbu712@gmail.com>
2026-02-18 21:09:26 +0545

    Logout Routing Done

--- dms_app/urls.py
+++ dms_app/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views.auth import register_view, login_view
+from .views.auth import register_view, login_view, logout_view
 from .views.main import home_view, admin_dashboard_view, approve_signup_request, reject_signup_request
 
 urlpatterns = [
     path("",home_view,name="home"),
     path("register/",register_view,name="register"),
     path("login/",login_view,name="login"),
+    path("logout/",logout_view,name="logout"),
     path("dashboard/admin/",admin_dashboard_view,name="admin-dashboard"),
     path("approve_signup_request/<int:request_id>/", approve_signup_request, name="approve_signup_request"),
     path("reject_signup_request/<int:request_id>/", reject_signup_request, name="reject_signup_request"),
