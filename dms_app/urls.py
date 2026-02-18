d1a44e33af45387f112dc0ccfe1a2dcedf04437d
bipinjlimbu <bipinlimbu712@gmail.com>
2026-02-18 18:30:43 +0545

    Admin Dashboard Setup Done

--- dms_app/urls.py
+++ dms_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from .views.auth import register_view, login_view
-from .views.main import home_view
+from .views.main import home_view, admin_dashboard_view
 
 urlpatterns = [
     path("",home_view,name="home"),
     path("register/",register_view,name="register"),
     path("login/",login_view,name="login"),
+    path("dashboard/admin/",admin_dashboard_view,name="admin-dashboard"),
 ]
--- dms_app/views/main.py
+++ dms_app/views/main.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home_view(request):
     return render(request,"main/home_page.html")
+
+def admin_dashboard_view(request):
+    return render(request,"main/admin_dashboard.html")
