a1f8df4fd7df14216324322f1de8e9f0a12c4297
bipinjlimbu <bipinlimbu712@gmail.com>
2026-02-21 18:40:58 +0545

    Profile Page Setup Done

--- dms_app/views/profile.py
+++ dms_app/views/profile.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from ..models import User, NGOProfile, DonorProfile
 import re
+
+@login_required
+def profile_view(request, user_id):
+    user = get_object_or_404(User, id=user_id)
+    return render(request, 'main/profile_page.html', {'user': user})
 
 @login_required
 def edit_profile_view(request, user_id):
