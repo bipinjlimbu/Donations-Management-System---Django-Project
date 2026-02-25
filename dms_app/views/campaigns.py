8cf339a6ba7115ac794458e4db80c16f4c1bd7a4
bipinjlimbu <bipinlimbu712@gmail.com>
2026-02-25 19:36:30 +0545

    Create Campaign Page Setup Done

--- dms_app/views/campaigns.py
+++ dms_app/views/campaigns.py
@@ -2,3 +2,6 @@ from django.shortcuts import render, redirect
 
 def campaigns_page_view(request):
     return render(request,"main/campaigns_page.html")
+
+def create_campaign_view(request):
+    return render(request,"main/create_campaign_page.html")
