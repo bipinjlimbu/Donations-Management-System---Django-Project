9583cbd279c94d1d3aea91f78dc8c2d389e2d4a3
bipinjlimbu <bipinlimbu712@gmail.com>
2026-02-25 08:33:57 +0545

    Urls Added for Approve and Reject Profile Changes

--- dms_app/urls.py
+++ dms_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views.auth import register_view, login_view, logout_view
 from .views.main import home_view, contact_view, about_view, admin_dashboard_view, approve_signup_request, reject_signup_request
-from .views.profile import profile_view, edit_profile_view
+from .views.profile import profile_view, edit_profile_view, approve_pending_changes, reject_profile_changes
 
 urlpatterns = [
     path("",home_view,name="home"),
@@ -15,4 +15,6 @@ urlpatterns = [
     path("dashboard/admin/",admin_dashboard_view,name="admin-dashboard"),
     path("approve_signup_request/<int:request_id>/", approve_signup_request, name="approve_signup_request"),
     path("reject_signup_request/<int:request_id>/", reject_signup_request, name="reject_signup_request"),
+    path("approve_profile_change/<int:user_id>/", approve_pending_changes, name="approve_profile_change"),
+    path("reject_profile_change/<int:user_id>/", reject_profile_changes, name="reject_profile_change"),
 ]
