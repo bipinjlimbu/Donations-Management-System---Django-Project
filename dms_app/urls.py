51b0c1ec6d1bb3e03ffd704e290b5fa74f5c223c
bipinjlimbu <bipinlimbu712@gmail.com>
2026-02-25 12:46:45 +0545

    Campaigns Page Routing Done

--- dms_app/urls.py
+++ dms_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views.auth import register_view, login_view, logout_view
 from .views.main import home_view, contact_view, about_view, admin_dashboard_view, approve_signup_request, reject_signup_request
 from .views.profile import profile_view, edit_profile_view, approve_pending_changes, reject_profile_changes
-from .views.campaign import campaign_view
+from .views.campaigns import campaigns_page_view
 
 urlpatterns = [
     path("",home_view,name="home"),
@@ -14,7 +14,7 @@ urlpatterns = [
     path("profile/<int:user_id>/", profile_view, name="profile"),
     path("profile/edit/<int:user_id>/", edit_profile_view, name="edit-profile"),
     path("dashboard/admin/",admin_dashboard_view,name="admin-dashboard"),
-    path("campaigns/",campaign_view,name="campaigns"),
+    path("campaigns/",campaigns_page_view,name="campaigns"),
     path("approve_signup_request/<int:request_id>/", approve_signup_request, name="approve_signup_request"),
     path("reject_signup_request/<int:request_id>/", reject_signup_request, name="reject_signup_request"),
     path("approve_profile_change/<int:user_id>/", approve_pending_changes, name="approve_profile_change"),
