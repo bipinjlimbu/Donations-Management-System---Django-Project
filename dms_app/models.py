5dadc13a79c6070cf3ea250e8ddfba2cef3c229f
bipinjlimbu <bipinlimbu712@gmail.com>
2026-03-03 19:57:51 +0545

    Donation Model Updated

--- dms_app/models.py
+++ dms_app/models.py
@@ -130,7 +130,9 @@ class Donation(models.Model):
         REJECTED = 'REJECTED', 'Rejected'
         
     donor = models.ForeignKey(User, on_delete=models.SET_NULL, limit_choices_to={'role': User.Role.DONOR}, null=True, blank=True)
+    donor_name = models.CharField(max_length=100)
     campaign = models.ForeignKey(Campaign, on_delete=models.SET_NULL, null=True, blank=True)
+    campaign_title = models.CharField(max_length=200)
     quantity = models.PositiveIntegerField()
     status = models.CharField(max_length=15, choices=Status.choices, default=Status.PENDING)
     delivered_at = models.DateTimeField(auto_now_add=True)
