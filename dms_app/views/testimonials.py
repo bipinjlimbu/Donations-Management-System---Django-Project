3fcfd7f468b8190c0b0b8110d417eb05b5e5d86b
bipinjlimbu <bipinlimbu712@gmail.com>
2026-03-09 20:16:36 +0545

    Create Testimonials Page Setup Done

--- dms_app/views/testimonials.py
+++ dms_app/views/testimonials.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def testimonials_view(request):
     return render(request, "main/testimonials_page.html")
+
+def create_testimonial_view(request):
+    return render(request, "main/create_testimonial_page.html")
