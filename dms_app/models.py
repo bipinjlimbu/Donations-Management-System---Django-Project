4aae8c4c560e604ae9f05efb559c1a96a35cee0a
bipinjlimbu <bipinlimbu712@gmail.com>
2026-03-10 10:05:31 +0545

    Status Added in Testimonial Model

--- dms_app/models.py
+++ dms_app/models.py
@@ -155,8 +155,14 @@ class Feedback(models.Model):
     submitted_at = models.DateTimeField(auto_now_add=True)
     
 class Testimonial(models.Model):
+    class Status(models.TextChoices):
+        PENDING = 'PENDING', 'Pending'
+        APPROVED = 'APPROVED', 'Approved'
+        REJECTED = 'REJECTED', 'Rejected'
+        
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     message = models.TextField()
     rating = models.PositiveIntegerField(default=5)
+    status = models.CharField(max_length=10, choices=Status.choices, default=Status.PENDING)
     submitted_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
