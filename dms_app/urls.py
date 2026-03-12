4ff486d8d97e30bbc6407e70a3b28db62fcaa447
bipinjlimbu <bipinlimbu712@gmail.com>
2026-03-12 18:31:11 +0545

    Approve and Reject Campaign Change Urls Added

--- dms_app/urls.py
+++ dms_app/urls.py
@@ -5,7 +5,7 @@ from .views.admin_dashboard import admin_dashboard_view, approve_signup_request,
 from .views.donor_dashboard import donor_dashboard_view, donate_view
 from .views.ngo_dashboard import ngo_dashboard_view, approve_donation_view, reject_donation_view
 from .views.profile import profile_view, edit_profile_view, approve_profile_changes, reject_profile_changes
-from .views.campaigns import campaigns_page_view, create_campaign_view, edit_campaign_view, delete_campaign_view, approve_campaign_request, reject_campaign_request, single_campaign_page_view
+from .views.campaigns import campaigns_page_view, create_campaign_view, edit_campaign_view, delete_campaign_view, approve_campaign_request, reject_campaign_request, single_campaign_page_view, approve_campaign_changes, reject_campaign_changes
 from .views.testimonials import testimonials_view, create_testimonial_view, approve_testimonial_view, reject_testimonial_view, edit_testimonial_view, delete_testimonial_view
 
 urlpatterns = [
@@ -40,6 +40,8 @@ urlpatterns = [
     path("reject_profile_change/<int:user_id>/", reject_profile_changes, name="reject_profile_change"),
     path("approve_campaign_request/<int:campaign_id>/", approve_campaign_request, name="approve_campaign_request"),
     path("reject_campaign_request/<int:campaign_id>/", reject_campaign_request, name="reject_campaign_request"),
+    path("approve_campaign_change/<int:campaign_id>/", approve_campaign_changes, name="approve_campaign_change"),
+    path("reject_campaign_change/<int:campaign_id>/", reject_campaign_changes, name="reject_campaign_change"),
     path("approve_testimonial/<int:testimonial_id>/", approve_testimonial_view, name="approve_testimonial"),
     path("reject_testimonial/<int:testimonial_id>/", reject_testimonial_view, name="reject_testimonial"),
 ]
