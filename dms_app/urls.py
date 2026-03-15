bd250d49664c9f31f4cc17301558c687cab97968
bipinjlimbu <bipinlimbu712@gmail.com>
2026-03-15 18:57:26 +0545

    Notifications Url Changed

--- dms_app/urls.py
+++ dms_app/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from .views.auth import register_view, login_view, logout_view
-from .views.main import home_view, contact_view, about_view, notifications_view
+from .views.main import home_view, contact_view, about_view
 from .views.admin_dashboard import admin_dashboard_view, approve_signup_request, reject_signup_request, mark_feedback_toggle, delete_feedback_view
 from .views.donor_dashboard import donor_dashboard_view, donate_view
 from .views.ngo_dashboard import ngo_dashboard_view, approve_donation_view, reject_donation_view
 from .views.profile import profile_view, edit_profile_view, approve_profile_changes, reject_profile_changes, delete_profile_view
 from .views.campaigns import campaigns_page_view, create_campaign_view, edit_campaign_view, delete_campaign_view, approve_campaign_request, reject_campaign_request, single_campaign_page_view, approve_campaign_changes, reject_campaign_changes
 from .views.testimonials import testimonials_view, create_testimonial_view, approve_testimonial_view, reject_testimonial_view, edit_testimonial_view, delete_testimonial_view, approve_testimonial_change, reject_testimonial_change
+from .views.notifications import notifications_view
 
 urlpatterns = [
     path("",home_view,name="home"),
