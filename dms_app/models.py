de883a4fbeeba06406314b3f0759c35040c8fb01
bipinjlimbu <bipinlimbu712@gmail.com>
2026-03-15 08:34:52 +0545

    Notification Model Updated

--- dms_app/models.py
+++ dms_app/models.py
@@ -189,6 +189,7 @@ class PendingTestimonial(models.Model):
     
 class Notification(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notifications')
+    title = models.CharField(max_length=100)
     message = models.CharField(max_length=255)
     is_read = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
